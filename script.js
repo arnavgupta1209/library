e8854224a73a28791a7a7ef1bc13317fa7939637
Arnav Gupta <arnavgupta1209@gmail.com>
2022-01-16 18:50:45 +0530

    added remove and read buttons, read doesnt work

--- script.js
+++ script.js
@@ -13,27 +13,37 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function updateDisplay() {
+
+
     // first remove all cards from dom
     document.querySelectorAll(".card").forEach(e => e.remove());
+
+
     // add cards from array books into dom
     for (let i in library) {
+
         let newdiv = document.createElement("div");
         newdiv.classList.add("card");
+        //newdiv.dataset.index = i;
+
         //make divs to add content using cardtitle and cardcontent
         let cardtitle = document.createElement("div");
         cardtitle.classList.add("cardtitle");
         cardtitle.textContent = `${library[i].title}`
         newdiv.appendChild(cardtitle);
+
         //make content, first author
         let cardauthor = document.createElement("div");
         cardauthor.classList.add("cardcontent");
         cardauthor.textContent = `by ${library[i].author}`
         newdiv.appendChild(cardauthor);
+
         //make content, pages
         let cardpages = document.createElement("div");
         cardpages.classList.add("cardcontent");
         cardpages.textContent = `${library[i].pages} pages long`
         newdiv.appendChild(cardpages);
+
         //make content, read or not
         let cardread = document.createElement("div");
         cardread.classList.add("cardcontent");
@@ -43,12 +53,43 @@ function updateDisplay() {
             cardread.textContent = `not read`
         }
         newdiv.appendChild(cardread);
+
+        //make the buttoncontainer div with cardcontent class
+        let buttoncontainer = document.createElement("div");
+        buttoncontainer.classList.add("cardcontent");
+        //make the removebutton and readbutton
+        let removebutton = document.createElement("button");
+        removebutton.classList.add("removebutton");
+        removebutton.textContent = "remove";
+
+        let readbutton = document.createElement("button");
+        readbutton.textContent = "toggle read";
+        if (library[i].read === true) {
+            readbutton.classList.add("readbutton");
+        } else {
+            readbutton.classList.add("notreadbutton");
+        }
+
+
+        //append buttons to container
+        buttoncontainer.appendChild(readbutton);
+        buttoncontainer.appendChild(removebutton);
+
+        //append container to newdiv
+        newdiv.appendChild(buttoncontainer);
+
         //append completed content into card
         document.querySelector(".content").appendChild(newdiv);
+
+        // making event listener for remove
+        removebutton.addEventListener("click", () => {
+            library.splice(i,1);
+            updateDisplay();
+        });
+
     }
 }
 
-addBookToLibrary("harry potter", "jrr martin", "456", false);
 
 // make function to take data from form and use addbooktolibrary 
 function addFromForm() {
